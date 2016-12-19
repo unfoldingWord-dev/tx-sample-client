7b5c0dc99ae9524a10f2f2fc5c64c794d442d62c
Gogs <gogs@fake.local>
2016-12-19 08:14:42 -0700

    Update for Travis

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(here, "README.rst"), "r") as f:
+    long_description = f.read()
+
+setup(
+    name="tx-sample-client",
+    version="1.0.0",
+    description="USFM-to-HTML conversion",
+    long_description=long_description,
+    url="https://github.com/unfoldingWord-dev/tx-sample-client",
+    author="unfoldingWord",
+    author_email="ethantkoenig@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    keywords=["usfm", "html"],
+    packages=find_packages(),
+    install_requires=["future"],
+    test_suite="tests"
+)
